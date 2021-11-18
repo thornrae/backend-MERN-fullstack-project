ad3a4fa6dc753a6e4b3a17534634d990808d8e26
Taylor Thornton <thornrae@gmail.com>
2021-11-17 20:48:19 -0800

    conditional routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,35 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('get request in places');
-  res.json({message: 'it works'});
+const DUMMY_PLACES = [
+  {
+    id: 'p1', 
+    title: 'empire state building', 
+    description: 'tall ass building',
+    location: {
+      lat: 40.7484405,
+      lon: -73.9878531
+    }, 
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
+]
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  })
+  res.json({place: place}); //({place})
 });
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+  res.json({place})
+});
+
 
 module.exports = router;

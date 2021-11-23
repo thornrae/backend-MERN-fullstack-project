e825fd7ad43463d022903652830eaf9e9ee19499
Taylor Thornton <thornrae@gmail.com>
2021-11-22 18:34:07 -0800

    handling errors

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ const app = express();
 
 app.use('/api/places', placesRoutes);
 
+//MW will run only if the previous function returns an error. Express recognizes when there are 4 parameters instead of the usual 3 as error handling middleware
+app.use((error, req, res, next) => {
+  if(res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({message: error.message || 'an unknown error occurred'});
+});
+
+
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -20,7 +20,13 @@ router.get('/:pid', (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find(p => {
     return p.id === placeId;
-  })
+  });
+
+  if(!place){
+    const error = new Error('could not find place for provided place id');
+    error.code = 404;
+    throw error
+  } 
   res.json({place: place}); //({place})
 });
 
@@ -29,6 +35,12 @@ router.get('/user/:uid', (req, res, next) => {
   const place = DUMMY_PLACES.find(p => {
     return p.creator === userId;
   });
+  if(!place){
+    const error = new Error('could not find place for provided user id');
+    error.code = 404;
+    return next(error);
+  }
+
   res.json({place})
 });
 

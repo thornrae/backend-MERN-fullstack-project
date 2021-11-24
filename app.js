99ea5aae40058922f7670804699fc598e049dd4a
Taylor Thornton <thornrae@gmail.com>
2021-11-23 18:25:28 -0800

    adding patch routes to update places

--- app.js
+++ app.js
@@ -13,10 +13,8 @@ app.use('/api/places', placesRoutes);
 app.use((req, res, next) => {
   const error = new HttpError('could not find this route', 404);
   throw error;
-
 });
 
-//MW will run only if the previous function returns an error. Express recognizes when there are 4 parameters instead of the usual 3 as error handling middleware
 app.use((error, req, res, next) => {
   if(res.headerSent) {
     return next(error);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -10,6 +10,11 @@ router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
-router.post('/', placesControllers.createPlace)
+router.post('/', placesControllers.createPlace);
+
+router.patch('/:pid', placesControllers.updatePlace);
+
+router.delete('/:pid', placesControllers.deletePlace);
+
 
 module.exports = router;

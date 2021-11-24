c1e9fffe26c4e61327034e05ce60e93b89be9930
Taylor Thornton <thornrae@gmail.com>
2021-11-23 18:09:30 -0800

    handling errors for unsupported routes

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-// const bodyParser = require('body-parser');
 
 app.use(express.json());
-// app.use(bodyParser.OptionsJson());
-// app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
+
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('could not find this route', 404);
+  throw error;
+
+});
 
 //MW will run only if the previous function returns an error. Express recognizes when there are 4 parameters instead of the usual 3 as error handling middleware
 app.use((error, req, res, next) => {

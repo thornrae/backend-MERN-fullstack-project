cf5865a27efb0a5370ae4159929e4ac3fa990a86
Taylor Thornton <thornrae@gmail.com>
2021-12-01 21:45:55 -0800

    login feature

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -59,13 +59,25 @@ const signup = async (req, res, next) => {
   res.status(201).json({user: createdUser.toObject({ getters: true })})
 }
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
+
   const { email, password} = req.body;
   
-  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
-  if(!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError('could not id user, creds wrong', 401);
+  let existingUser;
+  try{
+      existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError('login failed', 500);
+    return next(error);
   }
+
+  if(!existingUser || existingUser.password !== password) {
+    const error = new HttpError (
+      'invalid credentials couldnt log you in', 401
+    )
+    return next(error)
+  }
+
 
   res.json({message: 'logged in'})
 }

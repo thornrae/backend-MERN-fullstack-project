a11925c4e8e512e12219abd8efea53020799e333
Taylor Thornton <thornrae@gmail.com>
2021-12-04 15:37:04 -0800

    refactored getPlacesByUserId using populate method to access places collection

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -46,19 +46,20 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places 
+  // let places 
+  let userWithPlaces
   try{
-    places = await Place.find( {creator: userId});
+    userWithPlaces = await User.findById(userId).populate('places')
 
   } catch (err) {
     const error = new HttpError('fetching places failed try again later', 500);
     return next(error)
   }
 
-  if(!places || places.length === 0){
+  if(!userWithPlaces || userWithPlaces.places.length === 0){
     return next(new HttpError('could not find places for provided user id', 404));
   }
-  res.json({ places: places.map(place => place.toObject({ getters: true })) })
+  res.json({ places: userWithPlaces.places.map(place => place.toObject({ getters: true })) })
 };
 
 const createPlace = async (req, res, next) => {

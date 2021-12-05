f9c72d98f3997582e49b1a842d48950c61d4e088
Taylor Thornton <thornrae@gmail.com>
2021-12-04 17:44:36 -0800

    CORS error handling

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+  next();
+})
+
+app.use((req, res, next) => {
   const error = new HttpError('could not find this route', 404);
   throw error;
 });
@@ -27,7 +34,7 @@ app.use((error, req, res, next) => {
 });
 
 // const options = { useNewUrlParser: true, useUnifiedTopology: true};
-const connectUrl = 'mongodb+srv://trailer:rolyat@cluster0.cmthy.mongodb.net/places?retryWrites=true&w=majority';
+const connectUrl = 'mongodb+srv://trailer:rolyat@cluster0.cmthy.mongodb.net/mern?retryWrites=true&w=majority';
 
 const connectConfig = {
   useNewUrlParser: true, 
